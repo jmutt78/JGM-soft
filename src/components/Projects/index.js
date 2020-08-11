814e65ffeaa6978c4f1ab7ee737224b96cba05fc
jmutt78 <jmcintosh@docusites.com>
2020-08-11 15:57:21 -0700

    added 2 new projects

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,65 +1,81 @@
-import React from "react";
+import React from 'react';
 
-import Hover from "./Hover";
+import Hover from './Hover';
 
-import mattid from "../../images/mattid.jpg";
-import hike from "../../images/hike.jpg";
-import docusite from "../../images/docusite.jpg";
-import journal from "../../images/journal.jpg";
-import entra from "../../images/mountain.jpg";
-import ihmo from "../../images/ihmo.jpg";
-import tamilee from "../../images/tamilee.png";
-import { Root, SkillsContainer, SkillsColumn } from "./styles";
+import mattid from '../../images/mattid.jpg';
+import hike from '../../images/hike.jpg';
+import docusite from '../../images/docusite.jpg';
+import journal from '../../images/journal.jpg';
+import entra from '../../images/mountain.jpg';
+import ihmo from '../../images/ihmo.jpg';
+import sharp from '../../images/sharp.jpg';
+import shower from '../../images/shower.jpg';
+
+import { Root, SkillsContainer, SkillsColumn } from './styles';
 
 const Projects = () => {
   const myWorksection1 = [
     {
       image: entra,
-      title: "Entra",
+      title: 'Entra',
       body: `A powerful community of entrepreneurs that help each other learn and grow through a simple Question and Answer forum.`,
-      url: "https://entra.io/",
-      stack: "React, NextJS, graphQl, MongoDB, NodeJS",
-      git: "https://github.com/jmutt78/Entra-Backend"
+      url: 'https://entra.io/',
+      stack: 'React, NextJS, graphQl, MongoDB, NodeJS',
+      git: 'https://github.com/jmutt78/Entra-Backend',
     },
     {
       image: mattid,
-      title: "Matti D",
-      body: "An e-commerce website for a retail boutique.",
-      url: "https://mattidstyle.com",
-      stack: "Shopify"
+      title: 'Matti D',
+      body: 'An e-commerce website for a retail boutique.',
+      url: 'https://mattidstyle.com',
+      stack: 'Shopify',
     },
     {
       image: ihmo,
-      title: "Integrative Health Management",
+      title: 'Integrative Health Management',
       body:
-        "An informational business website that allows users to book appointments, download forms, and watch informative videos.",
-      url: "https://adrianak.netlify.app/",
-      stack: "Gatsby.Js, Wordpress headless CMS"
+        'An informational business website that allows users to book appointments, download forms, and watch informative videos.',
+      url: 'https://adrianak.netlify.app/',
+      stack: 'Gatsby.Js, Wordpress headless CMS',
     },
     {
       image: docusite,
-      title: "DocuSite",
-      body: "A business website for a construction application.",
-      url: "https://docusites.com",
-      stack: "Wordpress"
+      title: 'DocuSite',
+      body: 'A business website for a construction application.',
+      url: 'https://docusites.com',
+      stack: 'Wordpress',
     },
 
     {
       image: journal,
-      title: "Daily Journal",
+      title: 'Daily Journal',
       body: `Project that lets users record a simple journal.`,
-      url: "https://my-journal-app123.herokuapp.com",
-      stack: "React, Redux, MongoDB, Express, NodeJS",
-      git: "https://github.com/jmutt78/15-Minute-Journal"
+      url: 'https://my-journal-app123.herokuapp.com',
+      stack: 'React, Redux, MongoDB, Express, NodeJS',
+      git: 'https://github.com/jmutt78/15-Minute-Journal',
     },
     {
       image: hike,
-      title: "Trail Finder API",
+      title: 'Trail Finder API',
       body: `Data populated and cached with REST API calls to trails database.`,
-      url: "https://jmutt78.github.io/Hiking-API-Hack/",
-      stack: "HTML, CSS, JavaScript, jQuery",
-      git: "https://github.com/jmutt78/Hiking-API-Hack"
-    }
+      url: 'https://jmutt78.github.io/Hiking-API-Hack/',
+      stack: 'HTML, CSS, JavaScript, jQuery',
+      git: 'https://github.com/jmutt78/Hiking-API-Hack',
+    },
+    {
+      image: sharp,
+      title: 'Sharp On-demand Video Doctor Visit',
+      body: `Incorporating React with the legacy codebase.`,
+      url: 'https://www.sharp.com/get-care/virtual-urgent-care.cfm',
+      stack: 'HTML, CSS, React, PHP',
+    },
+    {
+      image: shower,
+      title: 'Shower Her',
+      body: `A Custom web application for event shower invitations and e-commerce utilizing Wordpress as a CMS.`,
+      url: 'https://shower-her-staging.herokuapp.com',
+      stack: 'React, NextJS, Wordpress, PHP, Heroku',
+    },
   ];
 
   return (
