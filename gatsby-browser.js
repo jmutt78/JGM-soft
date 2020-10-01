b1cc520b1851f61d9334a974e34dabb3a4442875
jmutt78 <jmcintosh@docusites.com>
2020-10-01 13:56:49 -0700

    flicekr

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import Layout from './src/components/layout.js';
-
 import './node_modules/bootstrap/dist/css/bootstrap.css';
+
+import Layout from './src/components/layout.js';
 
 export function wrapPageElement({ element, props }) {
   return <Layout {...props}>{element}</Layout>;
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,42 +1,42 @@
-import { Link } from "gatsby";
-import PropTypes from "prop-types";
-import React from "react";
+import { Link } from 'gatsby';
+import PropTypes from 'prop-types';
+import React from 'react';
 
-import HeaderModal from "./HeaderModal";
-import Logo from "../../../assets/logo.svg";
-import Calendly from "../Calendly";
+import HeaderModal from './HeaderModal';
+import Logo from '../../../assets/logo.svg';
+import Calendly from '../Calendly';
 
-import { Root, NavFlex, NavLinks, BlogLink } from "./styles";
+import { Root, NavFlex, NavLinks, BlogLink } from './styles';
 
 const Header = ({ siteTitle }) => (
-  <header>
+  <>
     <Root>
       <NavFlex>
-        <Link to="/">
+        <Link to='/'>
           <Logo />
         </Link>
         <NavLinks>
-          <HeaderModal buttonLabel="LETS CHAT">
+          <HeaderModal buttonLabel='LETS CHAT'>
             <Calendly />
           </HeaderModal>
 
           <BlogLink>
-            <Link to="/blog">
+            <Link to='/blog'>
               <h3>BLOG</h3>
             </Link>
           </BlogLink>
         </NavLinks>
       </NavFlex>
     </Root>
-  </header>
+  </>
 );
 
 Header.propTypes = {
-  siteTitle: PropTypes.string
+  siteTitle: PropTypes.string,
 };
 
 Header.defaultProps = {
-  siteTitle: ``
+  siteTitle: ``,
 };
 
 export default Header;
